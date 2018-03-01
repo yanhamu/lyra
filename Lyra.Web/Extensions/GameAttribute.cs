ee4bb512e670b0e89ae622c0153c569ae6828aee
Tomas Kriska <kriskatomas360@gmail.com>
2018-03-01 21:47:34 +0100

    implemented game attribute

--- Lyra.Web/Extensions/GameAttribute.cs
+++ Lyra.Web/Extensions/GameAttribute.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Lyra.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 
 namespace Lyra.Web.Extensions
 {
     public class GameAttribute : ActionFilterAttribute
     {
+        private readonly PlayerRepository playerRepository;
+
+        public GameAttribute(PlayerRepository playerRepository)
+        {
+            this.playerRepository = playerRepository;
+        }
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            throw new NotImplementedException();
+            var playerId = context.HttpContext.Request.Cookies["pid"];
+            if (playerId == null)
+            {
+                context.Result = new RedirectToActionResult("Index", "NationSetup", null);
+            }
+
+            var id = Guid.Parse(playerId);
+            var userId = context.HttpContext.User.Identity.Name;
+
+            var player = playerRepository.Get(id);
+            if (player.UserId != userId)
+            {
+                context.Result = new StatusCodeResult(400);
+            }
         }
     }
 }
